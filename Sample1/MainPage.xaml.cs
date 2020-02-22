a1e61a102bfb1e1515f9a84eb8e73df7c17184e9
Tyler K <tkelly10@my.wctc.edu>
2020-02-21 18:12:51 -0600

    Morse something or another

---
+++ Sample1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sample1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        string entryTxt = "";
+        string outText = "";
+        void ClickDot(object sender, System.EventArgs e)
+        {
+
+            entryTxt += ".";
+        }
+        void ClickDash(object sender, System.EventArgs e)
+        {
+            entryTxt += "-";
+        }
+
+        void ClickSpace(object sender, System.EventArgs e)
+        {
+            char text = Morse.MorseCoder(entryTxt);
+            outText += text;
+            entryTxt = "";
+            output.Text = outText;
+        }
+
+    }
+}
